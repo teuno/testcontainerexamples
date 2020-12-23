fe891f183c82a669a8754b3ba10cb7b664a65175
thooijer <thooijer@quintor.nl>
2020-12-23 17:01:08 +0100

    initialize project

---
+++ testcontainers/src/test/java/nl/teuno/testcontainers/TestcontainersApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.teuno.testcontainers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestcontainersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
